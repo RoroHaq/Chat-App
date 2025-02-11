17aed5afec4b38bd337531080b3e847867681514
Safin Haque <safinhq13@gmail.com>
2025-02-10 20:29:08 -0500

    included proper project

---
+++ FlashLang/FlashLang/src/main/java/com/example/FlashLang/FlashLangApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlashLang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashLangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashLangApplication.class, args);
+	}
+
+}
---
+++ FlashLang/FlashLang/src/test/java/com/example/FlashLang/FlashLangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlashLang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashLangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
