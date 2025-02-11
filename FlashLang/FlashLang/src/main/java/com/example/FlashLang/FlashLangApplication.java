26f2fb3e2a545debce66e7e0e360952eb114cc6b
Safin Haque <safinhq13@gmail.com>
2025-02-10 21:36:31 -0500

    add changes

--- FlashLang/FlashLang/src/main/java/com/example/FlashLang/FlashLangApplication.java
+++ FlashLang/FlashLang/src/main/java/com/example/FlashLang/FlashLangApplication.java
@@ -2,12 +2,17 @@ package com.example.FlashLang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FlashLangApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlashLangApplication.class, args);
 	}
-
+	
+	public String hello(){
+		return "Hello World";
+	}
 }
